eb052f9acc851259fe63ce0e96da907f64f3d863
Jasrel Roby Peralta <jtperalta1@up.edu.ph>
2022-11-07 08:08:59 +0000

    feat: add login

--- src/app.js
+++ src/app.js
@@ -2,20 +2,42 @@ import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
 import openAPIGlue from 'fastify-openapi-glue';
 import swagger from '@fastify/swagger';
+import cookie from '@fastify/cookie';
+import session from '@fastify/secure-session';
+import jwt from '@fastify/jwt';
 import { Service } from './services/index.js';
+import { Security } from './security/index.js';
 import { specification } from './specification/index.js';
+
 const prefix = '/api';
 
 export async function build () {
   // initialize fastify
   const fastify = Fastify({ logger: true });
+
+  fastify.register(cookie);
+  fastify.register(session, {
+    secret: 'A very long string for the secret that should work',
+    salt: '1234567890123456',
+    cookie: {
+      httpOnly: true,
+      maxAge: 60 * 60
+    }
+  });
+
+  fastify.register(jwt, {
+    secret: 'This is a very long string that will be used for the jwt secret'
+  });
+
   fastify.register(sensible);
 
   const service = new Service();
+  const securityHandlers = new Security(fastify);
 
   const openAPIGlueOptions = {
     specification,
     service,
+    securityHandlers,
     prefix
   };
 
@@ -27,23 +49,6 @@ export async function build () {
 
   fastify.register(swagger, swaggerOptions);
   fastify.register(openAPIGlue, openAPIGlueOptions);
-
-  // fastify.get(prefix, general);
-
-  // // create todo
-  // fastify.post(`${prefix}/todo`, createTodo);
-
-  // // get many todo
-  // fastify.get(`${prefix}/todo`, getManyTodo);
-
-  // // get one todo
-  // fastify.get(`${prefix}/todo/:todoId`, getTodo);
-
-  // // update todo
-  // fastify.put(`${prefix}/todo/:todoId`, updateTodo);
-
-  // // delete a todo
-  // fastify.delete(`${prefix}/todo/:todoId`, deleteTodo);
 
   return fastify;
 }
